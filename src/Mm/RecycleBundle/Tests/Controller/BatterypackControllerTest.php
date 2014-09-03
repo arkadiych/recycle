b9ef262f2456d7fc64837a5e7d363a2a9f5af28f
a.chyzhov <arkadiych@users.noreply.github.com>
2014-09-03 18:54:18 +0300

    #7 optimized test

--- src/Mm/RecycleBundle/Tests/Controller/BatterypackControllerTest.php
+++ src/Mm/RecycleBundle/Tests/Controller/BatterypackControllerTest.php
@@ -6,70 +6,39 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class BatterypackControllerTest extends WebTestCase
 {
-    private $client;
+    private $inputValues = array(
+        array(
+            'batterypack[type]' => 'AA',
+            'batterypack[count]' => 4
+        ),
+        array(
+            'batterypack[type]' => 'AAA',
+            'batterypack[count]' => 3
+        ),
+        array(
+            'batterypack[type]' => 'AA',
+            'batterypack[count]' => 1
+        )
+    );
 
-    public static function setUpBeforeClass()
+    public function testStatistic()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/batterypack/add');
+        $btn = $crawler->selectButton('batterypack_save');
 
-        $btn = $crawler->selectButton('form_save');
+        foreach($this->inputValues as $val) {
+            $form = $btn->form($val);
+            $client->submit($form);
+        }
 
-        $form = $btn->form(array(
-            'form[type]' => 'AA',
-            'form[count]' => 4
-        ));
-        $client->submit($form);
-
-        $form = $btn->form(array(
-            'form[type]' => 'AAA',
-            'form[count]' => 3
-        ));
-        $client->submit($form);
-
-        $form = $btn->form(array(
-            'form[type]' => 'AA',
-            'form[count]' => 1
-        ));
-        $client->submit($form);
-    }
-
-    public function setUp()
-    {
-        $this->client = static::createClient();
-    }
-
-    public function tearDown()
-    {
-        unset($this->client);
-    }
-
-    public function testRowsCont()
-    {
-        $crawler = $this->client->request('GET', '/');
+        $crawler = $client->request('GET', '/');
         $this->assertCount(2, $crawler->filter('table.battery-stats tbody tr'));
+
+        $this->assertCount(1, $crawler->filterXPath(
+            "//tr[td[normalize-space(text())='AA'] and td[normalize-space(text())='5']]"));
+        $this->assertCount(1, $crawler->filterXPath(
+            "//tr[td[normalize-space(text())='AAA'] and td[normalize-space(text())='3']]"));
     }
-
-    /**
-    * @dataProvider provider
-    */
-    public function testTypeCount($type, $count, $pos)
-    {
-        //todo: you should combine this test method with "testRowsCont"
-        // also code from setUpBeforeClass should be moved into this test method
-        // you should take care of test's performance. In your case there are 15 ! requests to controller.
-        // the same functionality could be covered with only 5 calls: 1 - to render form first time, 3 - to submit data 3 times, 1 - to check statistics page
-        $crawler = $this->client->request('GET', '/');
-        $this->assertTrue($crawler->filter('table.battery-stats tbody td')->eq($pos)->text() == $count);
-    }
-
-    public function provider()
-    {
-      return array(
-        array('AA', 5, 1),
-        array('AAA', 3, 3)
-      );
-  }
-
 }
